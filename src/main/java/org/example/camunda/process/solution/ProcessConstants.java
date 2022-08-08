a87259e271152bd56f4445d0f1d5ea1b868fcdbc
Dave Paroulek <david.paroulek@camunda.com>
2022-08-08 13:56:23 -0400

    set process id

--- src/main/java/org/example/camunda/process/solution/ProcessConstants.java
+++ src/main/java/org/example/camunda/process/solution/ProcessConstants.java
@@ -2,5 +2,6 @@ package org.example.camunda.process.solution;
 
 public class ProcessConstants {
 
-  public static final String BPMN_PROCESS_ID = "camunda-process";
+  public static final String BPMN_PROCESS_ID = "Process_feelTutorial";
+
 }
