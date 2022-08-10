c81aa2b83407c0053784483f793154b38d47c171
Philipp Ossler <philipp.ossler@gmail.com>
2022-08-10 13:45:29 +0200

    feat: try to enable CORS

--- src/main/java/org/example/camunda/process/solution/ProcessApplication.java
+++ src/main/java/org/example/camunda/process/solution/ProcessApplication.java
@@ -8,6 +8,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.support.MessageBuilder;
 
 @SpringBootApplication
 @EnableZeebeClient
@@ -21,9 +23,45 @@ public class ProcessApplication {
   }
 
   @Bean
-  public Function<FeelEvaluationRequest, String> startProcess() {
-    return request ->
-        zeebeService.startProcess(
-            request.getExpression(), request.getContext(), request.getMetadata());
+  public Function<Message<?>, Message<?>> enableCors() {
+    return message -> {
+      System.out.println("> enable CORS");
+      System.out.println("> headers: " + message.getHeaders());
+      return MessageBuilder.fromMessage(message)
+          //          .withPayload(message.getPayload())
+          //          .copyHeaders(message.getHeaders())
+          .setHeader("Access-Control-Allow-Origin", "*")
+          .setHeader("Access-Control-Allow-Methods", "POST")
+          .build();
+    };
+  }
+
+  @Bean
+  public Function<FeelEvaluationRequest, String> evaluateExpression() {
+    return request -> {
+      System.out.println("> evaluateExpression");
+      return zeebeService.startProcess(
+          request.getExpression(), request.getContext(), request.getMetadata());
+    };
+  }
+
+  @Bean
+  public Function<Message<FeelEvaluationRequest>, Message<String>> startProcess() {
+    return message -> {
+      System.out.println("> startProcess");
+
+      final var request = message.getPayload();
+      final var result =
+          zeebeService.startProcess(
+              request.getExpression(), request.getContext(), request.getMetadata());
+
+      System.out.println(">>> headers: " + message.getHeaders());
+
+      return MessageBuilder.withPayload(result)
+          .copyHeaders(message.getHeaders())
+          .setHeader("Access-Control-Allow-Origin", "*")
+          .setHeader("Access-Control-Allow-Methods", "POST")
+          .build();
+    };
   }
 }
